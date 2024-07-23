2c7f7f8e914a7292ed9d4f455d53295a3a110bed
guiqwer <guilherme.souza0996@gmail.com>
2024-07-23 14:45:57 -0300

    test

--- lists/list3/program04.c
+++ lists/list3/program04.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+#define MAX_STRING_LENGTH 100
+
+void getInputString(char string1[], char string2[])
+{
+    printf("Enter the first string: ");
+    fgets(string1, MAX_STRING_LENGTH, stdin);
+
+    printf("Enter the second string: ");
+    fgets(string2, MAX_STRING_LENGTH, stdin);
+
+}
+
 int main(){
-    
+
+    char firstString[MAX_STRING_LENGTH];
+    char secondString[MAX_STRING_LENGTH];
+
+    getInputString(firstString, secondString);
+
+
 
     return 0;
 }
