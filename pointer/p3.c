35996c4f3dffced5bd62b3aa28a0c1f66f25af4a
guiqwer <guilherme.souza0996@gmail.com>
2024-07-29 15:26:58 -0300

    resolves p3.c in pointer

--- pointer/p3.c
+++ pointer/p3.c
@@ -5,7 +5,8 @@
 #define TAM 15
 #define MX 10
 
-int main(){
+int main()
+{
 
     int v[TAM];
     int num;
@@ -16,44 +17,59 @@ int main(){
     // Preencher o vetor
     srand(time(NULL));
 
-    for (int k=0; k<TAM; k++){
-        *(pv+k) = rand() % MX;
+    for (int k = 0; k < TAM; k++)
+    {
+        *(pv + k) = rand() % MX;
     }
 
     puts("Entre com o número a ser buscado no vetor: ");
-    scanf("%d",pnum);
+    scanf("%d", pnum);
 
     // Busca
     int *p = NULL;
-    for (int k=0; k<TAM; k++){
-        if (*pnum == *(pv+k)){
+    for (int k = 0; k < TAM; k++)
+    {
+        if (*pnum == *(pv + k))
+        {
             p = pv + k;
             break;
         }
     }
 
     // Imprimir
-    puts("Vetor gerado: "); 
-    for (int k=0; k<TAM; k++){
-        printf("[%p] %d\n",pv+k, *(pv+k));
+    puts("Vetor gerado: ");
+    for (int k = 0; k < TAM; k++)
+    {
+        printf("[%p] %d\n", pv + k, *(pv + k));
     }
 
     printf("\n");
-    (p)?printf("End. primeira ocorrência de %d no vetor: %p\n\n",*pnum, p):printf("%d não ocorre no vetor! \n",*pnum);
+    (p) ? printf("End. primeira ocorrência de %d no vetor: %p\n\n", *pnum, p) : printf("%d não ocorre no vetor! \n", *pnum);
 
     //======
-    int *pmm[2] = {NULL}; // menor, maior
+    int *pmm[2];    // menor e maior
+    int *pmin = pv; // menor
+    int *pmax = pv; // maior
 
-    pmm[0] = pv;
-    pmm[1] = pv;
-    for (int k=0; k<TAM; k++){
-        pmm[0] = (*pmm[0] > *(pv+k))?pv+k:pmm[0]; // menor
-        pmm[1] = (*pmm[1] < *(pv+k))?pv+k:pmm[1]; // maior
+    for (int k = 1; k < TAM; k++)
+    {
+        if (*(pv + k) < *pmin) // menor
+        {
+            pmin = pv + k;
+        }
+
+        if (*(pv + k) > *pmax) // maior
+        {
+            pmax = pv + k;
+        }
     }
 
-    printf("End. do menor: %p\n",*(pmm+0));
-    printf("End. do maior: %p\n",*(pmm+1));
+    // Atualizar o array de ponteiros
+    pmm[0] = pmin;
+    pmm[1] = pmax;
+
+    printf("End. do menor: %p com valor %d\n", *(pmm + 0), *(*(pmm + 0)));
+    printf("End. do maior: %p com valor %d\n", *(pmm + 1), *(*(pmm + 1)));
 
     return 0;
-
 }
