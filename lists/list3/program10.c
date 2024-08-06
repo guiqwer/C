8731f31019547d6f381981358be35f67626740f4
guiqwer <guilherme.souza0996@gmail.com>
2024-08-06 11:38:33 -0300

    question 10 resolves

--- lists/list3/program10.c
+++ lists/list3/program10.c
@@ -1,6 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(){
+#define TAM 3
+#define RANGE 10
+
+int main()
+{
+
+    int vec[TAM][TAM];
+
+    // recieve values of matrices
+
+    srand(time(NULL));
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            vec[i][j] = rand() % RANGE;
+        }
+    }
+
+    // print the rows and coluns
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            printf("%d ", vec[i][j]);
+        }
+        printf("\n"); // new row after the row of matrices
+    }
+
+    // count if the number of user are in matrices
+
+    int userChoice;
+    int count = 0;
+
+    puts("Enter a number for verify if its in the matrices: ");
+    scanf("%d", &userChoice);
+
+    for (int k = 0; k < TAM; k++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            if (userChoice == vec[k][j])
+            {
+                count += 1;
+            }
+            else
+            {
+                puts("The number not exist in the matrices.");
+                exit(1);
+            }
+        }
+    }
+
+    // output
+
+    printf("The number %d pops up %d times", userChoice, count);
 
     return 0;
 }
