c7b6d17c9011d8e7b20cd06d34d5065181e9fe7c
guiqwer <guilherme.souza0996@gmail.com>
2024-08-06 11:12:46 -0300

    question 9 resolves

--- lists/list3/program09.c
+++ lists/list3/program09.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+#define TAM 3
+
+int main()
+{
+
+    int vec[TAM][TAM];
+
+    // recieve values of matrices
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            printf("Type a number for the row  %d and colun %d: ", i, j);
+            scanf("%d", &vec[i][j]);
+        }
+    }
+
+    // print the rows and coluns
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            printf("%d ", vec[i][j]);
+        }
+        printf("\n"); // new row after the row of matrices
+    }
+
+    printf("\n");
+
+    // output the main diagonal
+
+    printf("The main diagonal: \n");
+    for (int k = 0; k < TAM; k++)
+    {
+        printf(" %d", vec[k][k]);
+    }
 
     return 0;
 }
