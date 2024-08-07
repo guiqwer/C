67ffcac4cab267de2891be0236d0db89ced22832
guiqwer <guilherme.souza0996@gmail.com>
2024-08-07 13:28:59 -0300

    question 4 resolves

---
+++ lists/list4/program04.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+#define RANGE 10
+
+int main()
+{
+
+    int *vec = NULL;
+    int qtd;
+
+    puts("Enter a number of array: ");
+    scanf("%d", &qtd);
+
+    if (!(vec = (int *)malloc(qtd * sizeof(int))))
+    {
+        puts("Memória indisponível");
+        exit(1);
+    }
+
+    srand(time(NULL));
+    for (int i = 0; i < qtd; i++)
+    {
+        *(vec + i) = rand() % RANGE;
+        printf("Adress [%p], Value [%d]\n", vec + i, *(vec + i));
+    }
+
+    free(vec);
+
+    return 0;
+}
