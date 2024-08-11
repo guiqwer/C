88100ef9f1f84ebebb183f576dcb7a344bd35818
guiqwer <guilherme.souza0996@gmail.com>
2024-08-11 15:25:31 -0300

    question 5 resolves

--- lists/list4/program05.c
+++ lists/list4/program05.c
@@ -10,12 +10,6 @@ int main()
     char *vec;
     int count = 0;
 
-    if (!(vec = (char *)malloc(LEN * sizeof(char))))
-    {
-        puts("Memória indisponível");
-        exit(1);
-    }
-
     puts("Enter the string: ");
     fgets(vec, LEN, stdin);
 
@@ -26,7 +20,7 @@ int main()
 
     // output
 
-    printf("Length of the string: %d\n", count);
+    printf("Length of the string: %d\n", *pCount);
 
     free(vec);
 
